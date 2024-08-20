afc748e8a30f11fc60c75e5609637c122114570c
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-08-20 13:59:31 +0500

    Add files via upload

---
+++ module9lesson1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for i in functions:
+            results[i.__name__] = [i(int_list)]
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
